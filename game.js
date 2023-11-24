6e5a85196ef4eff6375b832605d8f4e293bd9b13
NathaRuiz <nruizm98@gmail.com>
2023-11-24 17:41:56 +0100

    implement new funtion js and completed the exercise

--- game.js
+++ game.js
@@ -9,27 +9,35 @@ const cross_counter = document.getElementById("cross_counter");
 let counter_f = "0";
 let counter_c = "0";
 
-function random_spin (min, max){
-    return Math.floor(Math.random() * (max - min + 1) + min)
+function random_result(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-spin_coin.addEventListener("click", () =>{
-    
-    if (random_spin(1,2) === 1) {
-        coin_content.textContent = "FACE";
-        counter_f ++;
-        face_counter.textContent = counter_f;
-    } 
-    else {
-        coin_content.textContent = "CROSS";
-        counter_c ++;
-        cross_counter.textContent = counter_c;
-    }
-    
-})
+function add_rotate_event(coin_side){
+    coin_content.classList.add(`${coin_side}_rotate`);
+    setTimeout(() => {
+    coin_content.textContent = coin_side;
+    coin_content.classList.remove(`${coin_side}_rotate`);
+  }, 1000);
+}
 
-restart_game.addEventListener("click", () =>{
-    coin_content.textContent = "?";
-    face_counter.textContent = 0;
-    cross_counter.textContent = 0;
-})
+
+spin_coin.addEventListener("click", () => {
+  if (random_result(0, 1) === 0) {
+    add_rotate_event("FACE");
+    counter_f++;
+    face_counter.textContent = counter_f;
+  } else {
+    add_rotate_event("CROSS");
+    counter_c++;
+    cross_counter.textContent = counter_c;
+  }
+});
+
+restart_game.addEventListener("click", () => {
+  coin_content.textContent = "?";
+  face_counter.textContent = 0;
+  cross_counter.textContent = 0;
+});
+
+
